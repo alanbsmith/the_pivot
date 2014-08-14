135e35d632e6d30668b196b7db2cc1237b012657
Tan Doan <tdoan87@gmail.com>
2014-08-14 15:58:42 -0600

    before adding index to emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true, length: { maximum: 30 }
   validates :last_name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
 end
