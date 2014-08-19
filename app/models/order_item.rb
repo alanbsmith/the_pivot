0dd7fb5229f1534176acd29ea3daa833be575c99
Tan Doan <tdoan87@gmail.com>
2014-08-18 18:40:21 -0600

    patched photo to image

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,9 +2,4 @@ class OrderItem < ActiveRecord::Base
   belongs_to :item
   belongs_to :order
 
-  def create
-    if current_item
-    current_order.order_items.create(order_id: , item_id)
-    current_order.order_items.
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# ActiveRecord::Schema.define(version: 20140814234107) do
-# ActiveRecord::Schema.define(version: 20140813222715) do
 ActiveRecord::Schema.define(version: 20140816191212) do
 
   create_table "addresses", force: true do |t|
