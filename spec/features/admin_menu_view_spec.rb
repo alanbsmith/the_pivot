e766a7e3d783cf9c5ea3d6612a35c2f4c76fad8f
Corey Davis <coreyd303@gmail.com>
2014-08-21 14:46:08 -0600

    all tests green

--- spec/features/admin_menu_view_spec.rb
+++ spec/features/admin_menu_view_spec.rb
@@ -99,6 +99,7 @@ describe "admin_menu", type: :feature do
     click_button('Update this Item')
 
     visit items_path
+    save_and_open_page
     expect(page).to_not have_content(deleted_item)
   end
 end
