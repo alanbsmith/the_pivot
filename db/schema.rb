25c00c01f15f749e2414f02a4923de410127a789
Corey Davis <coreyd303@gmail.com>
2014-08-26 10:58:52 -0600

    blow away address table

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826155812) do
-
-  create_table "addresses", force: true do |t|
-    t.integer  "user_id"
-    t.string   "street"
-    t.string   "city"
-    t.string   "state"
-    t.integer  "zipcode"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140826165500) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
