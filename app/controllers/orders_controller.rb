1866cc726eabbcf7cb3398d4253eadd4d09af5bb
Hilary Denton <Hilarydenton90@gmail.com>
2014-08-27 18:00:27 -0600

    stuff

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,7 @@ class OrdersController < ApplicationController
 
   before_action :set_cart,  only: [:new, :create]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_order
 
   def index
     if current_user && signed_in?
@@ -59,15 +60,15 @@ class OrdersController < ApplicationController
   end
 
 
-  def destroy
-    Order.find(session[:order_id]).destroy
-    session[:order_id] = nil
-    respond_to do |format|
-      format.html { redirect_to items_url,
-        notice: 'Your cart is currently empty' }
-        format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   Order.find(session[:order_id]).destroy
+  #   session[:order_id] = nil
+  #   respond_to do |format|
+  #     format.html { redirect_to items_url,
+  #       notice: 'Your cart is currently empty' }
+  #       format.json { head :no_content }
+  #   end
+  # end
 
   private
 
@@ -77,5 +78,10 @@ class OrdersController < ApplicationController
 
   def set_order
     @order = Order.find(params[:id])
+  end
+
+  def invalid_order
+    logger.error "Attempt to access invalid order #{params[:id]}"
+    redirect_to items_url, notice: 'Invalid order'
   end
 end
--- spec/features/order_view_spec.rb
+++ spec/features/order_view_spec.rb
@@ -1,6 +1,40 @@
 require 'feature_helper'
 
 RSpec.describe "the order view", type: :feature do
-  
 
+  context 'user is signed in' do
+    before(:each) do
+      @default_user = User.create(email: "user@example.com", password: "userpassword", password_confirmation: "userpassword",
+                        first_name: "user", last_name: "whatever", role: "default")
+      visit home_path
+    end
+
+    it 'creates an order' do
+      default_login
+
+      click_link('View Order')
+
+
+    end
+
+    it 'shows an order' do
+    end
+
+    it 'edits an order' do
+    end
+  end
+
+  context 'user is not signed in' do
+    it 'goes to menu when they try to access orders' do
+      visit orders_path
+
+      expect(current_path).to eq(items_path)
+    end
+
+    it 'goes to menu when they try to access an order' do
+      visit 'orders/1'
+
+      expect(current_path).to eq(items_path)
+    end
+  end
 end
