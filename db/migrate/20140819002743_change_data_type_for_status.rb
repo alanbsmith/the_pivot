554a0e090cd86221967a6dc5deccb332e44de736
Corey Davis <coreyd303@gmail.com>
2014-08-28 10:01:28 -0600

    ballz

--- db/migrate/20140819002743_change_data_type_for_status.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataTypeForStatus < ActiveRecord::Migration
-  def up
-    change_column :items, :status, :integer
-  end
-
-  def down
-    change_column :items, :status, :string
-  end
-end
