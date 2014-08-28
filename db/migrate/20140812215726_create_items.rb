c6ad3de91663228515e6fe15615ecede1597cd46
Horacio Chavez <horacio.hch@gmail.com>
2014-08-28 10:46:35 -0600

    Text all the things

--- db/migrate/20140812215726_create_items.rb
+++ db/migrate/20140812215726_create_items.rb
@@ -1,10 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.string  :title
+      t.text    :title
       t.text    :description
       t.integer :price
-      t.string  :photo
+      t.text    :photo
       t.integer :status
 
       t.timestamps
