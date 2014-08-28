a71ed5ba46fb6360f0c249aa54ce1fd04e876203
Horacio Chavez <horacio.hch@gmail.com>
2014-08-28 10:21:27 -0600

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828154833) do
+ActiveRecord::Schema.define(version: 20140827204345) do
 
   create_table "cart_items", force: true do |t|
     t.integer  "item_id"
