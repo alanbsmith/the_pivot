f5c49dfd6fdbad5fae0f1ac8c26add6683a86c16
Horacio Chavez <horacio.hch@gmail.com>
2014-09-10 21:52:53 -0600

    removed a typo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url  = 'http://example.com/login'
+    @url  = 'http://employme.com/login'
     mail(to: @user.email, subject: 'Welcome to EmployMe!')
   end
 end
