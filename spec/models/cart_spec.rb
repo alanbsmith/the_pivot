6bdba64b6d64e29165b8f59dd227e036617ff68a
Horacio Chavez <horacio.hch@gmail.com>
2014-09-11 11:07:31 -0600

    comments in cart spec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -6,14 +6,14 @@
 #     it 'calculates the total of an cart' do
 #       cart = FactoryGirl.create(:cart)
 #       item = FactoryGirl.create(:item)
-RSpec.describe Cart, :type => :model do
-  describe '#total_price' do
-    xit 'calculates the total of an cart' do
-      cart = FactoryGirl.create(:cart)
-      item = FactoryGirl.create(:item)
-
-
-#       cart.cart_items.create(cart: cart, item: item, quantity: 2)
+# RSpec.describe Cart, :type => :model do
+#   describe '#total_price' do
+#     xit 'calculates the total of an cart' do
+#       cart = FactoryGirl.create(:cart)
+#       item = FactoryGirl.create(:item)
+#
+#
+# #       cart.cart_items.create(cart: cart, item: item, quantity: 2)
 
 
 #       expect(cart.total_price).to eq(13.00)
