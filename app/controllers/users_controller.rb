5b0b677bcf154250b410e204cff6f9f661119a7c
Alan Smith <a.bax.smith@gmail.com>
2014-09-11 16:01:10 -0600

    refactored tests--all passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save!
       UserMailer.welcome_email(@user).deliver
       sign_in @user
       redirect_to @user
--- spec/features/user/user_view_spec.rb
+++ spec/features/user/user_view_spec.rb
@@ -17,74 +17,36 @@ describe 'the user view', type: :feature do
           expect(current_path).to eq(new_user_path)
         end
 
-        it 'can register a business user' do
+        it 'can register a business' do
           visit home_path
           click_link('Register Your Business')
           expect(current_path).to eq(new_user_path)
-          fill_in("email", with: "user22@example.com")
+          fill_in("Company name", with: "FedEx")
+          fill_in("email", with: "user@example.com")
           fill_in("First Name", with: "Bob")
           fill_in("Last Name", with: "Gu")
           fill_in("Password", with: "password")
           fill_in("Confirmation", with: "password")
-          click_link_or_button("Register Now")
-          expect(page).to have_content("Bob")
+          click_button("Register Now")
+          expect(current_path).to eq user_path(User.last)
         end
 
-        it 'can log in as business user' do
-          default_login
-          expect(current_path).to eq(home_path)
+        it 'can log in as business' do
+          user = User.new
+            user.company_name          = "FedEx"
+            user.first_name            = "Fred"
+            user.last_name             = "Rex"
+            user.email                 = "fredrex@fedex.com"
+            user.password              = "password"
+            user.password_confirmation = "password"
+          user.save
+
+          visit signin_path
+          fill_in("Email", with: user.email)
+          fill_in("Password", with: "password")
+          click_button("Signin")
+          expect(current_path).to eq user_path(user)
         end
-
-    # it 'can log out as default user' do
-    #   default_login
-    #   click_link('Signout')
-    #   expect(current_path).to eq(root_path)
-    # end
-
-    # it 'cannot access the administrator items page' do
-    #   default_login
-    #   visit administrator_items_path
-    #   expect(current_path).to_not be(administrator_items_path)
-    # end
-
-    # it 'default user cannot access dashboard link' do
-    #   default_login
-    #   expect(page).to_not have_link("Dashboard")
-    # end
-
-    # it 'default user has a profile link' do
-    #   default_login
-    #   expect(page).to have_link('Profile')
-    # end
-
-    # it 'default user can visit profile page' do
-    #   default_login
-    #   click_link('Profile')
-    #   expect(current_path).to eq(user_path(@default_user))
-    # end
-
-    # it 'profile page displays information about user' do
-    #   visit user_path(@default_user)
-    #   expect(page).to have_content(@default_user.first_name)
-    #   expect(page).to have_content(@default_user.last_name)
-    # end
-
-    # it 'default user has a view orders link' do
-    #   default_login
-    #   expect(page).to have_link('View Orders')
-    # end
-
-    # it 'view orders page should have all associated orders with current user' do
-    #   @default_user.orders.create
-    #   @default_user.orders.create
-    #   user_orders = @default_user.orders
-    #   default_login
-    #   click_link("View Orders")
-    #   expect(current_path).to eq(orders_path)
-    #   user_orders.each do |order|
-    #     expect(page).to have_content(order.created_at)
-    #   end
-    # end
+      end
+    end
   end
-  end
-end
