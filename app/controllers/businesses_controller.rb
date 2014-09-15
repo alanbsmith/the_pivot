fda339c2163bdfca331ac51013eaba3a775926d6
Corey Davis <coreyd303@gmail.com>
2014-09-15 13:42:57 -0600

    update biz controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,12 +1,22 @@
 class BusinessController< ApplicationController
   
   def index
+    @business = current_business(params[:id])
   end
 
   def new
+    @business = Business.new
   end
 
-  def show
+  def create
+    @business = Business.new(business_params)
+    if @business.save!
+      Mailer.welcome_email(@business).deliver
+      sign_in @business
+      redirect_to @business
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,4 +28,13 @@ class BusinessController< ApplicationController
   def destroy
   end
 
+  private
+
+  def business_params
+    params.require(:business).permit(:company_name,
+                                     :first_name,
+                                     :last_name,
+                                     :email,
+                                     )
+  end
 end
