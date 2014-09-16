3cd2fe4da5de6268080473c9d885b3b65647504e
Robert Gu <verygudhokie@gmail.com>
2014-09-16 07:56:20 -0600

    error and success alerts with creation of an account

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password,              presence: true, length: { minimum: 8 }
-  validates :password_confirmation, presence: true  
+  validates :password_confirmation, presence: true
 
   validates :first_name,            presence: true, length: { maximum: 30 }
   validates :last_name,             presence: true, length: { maximum: 30 }
