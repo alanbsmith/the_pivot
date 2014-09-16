12ccfa5403bbbc1768587dac3a54d8ba8c04fdf3
Alan Smith <a.bax.smith@gmail.com>
2014-09-16 15:02:38 -0600

    users can view their cart from a link on the navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  include CurrentCart
+
+  before_action :set_cart
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  end
 end
