7ecf3c6ea8ebbe6138339ac10999f8f6b6301925
Robert Gu <verygudhokie@gmail.com>
2014-09-17 07:28:18 -0600

    category title checks for uniqueness

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   has_many :categorizations
   has_many :listings, through: :categorizations
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
 end
