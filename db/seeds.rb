355c4891ba827005aab75bb337626b2390de501d
Robert Gu <verygudhokie@gmail.com>
2014-09-17 13:42:28 -0600

    faker works for listings, categories, and users'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 class Seed
   def initialize
     generate_users
+    generate_categories
+    generate_listings
   end
 
   def generate_users
@@ -11,7 +13,7 @@ class Seed
       password: "password",
       password_confirmation: "password"
       )
-    20.times do |i|
+    10.times do |i|
       user = User.create!(
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
@@ -22,7 +24,7 @@ class Seed
       puts "User #{i}: #{user.first_name} - #{user.email} created!"
     end
 
-    20.times do |i|
+    10.times do |i|
       user = User.create!(
         company_name: Faker::Company.name,
         first_name: Faker::Name.first_name,
@@ -35,6 +37,52 @@ class Seed
     end
 
   end
+
+  def generate_categories
+    categories = %w(Technology, Marketing, Communications, Education, Food Services,
+    Medical, Business, Science)
+    8.times do |i|
+      category = Category.create!(
+        title: categories[i],
+        description: Faker::Company.catch_phrase
+      )
+      puts "Category #{i}: #{category.title} - #{category.description} created!"
+    end
+  end
+
+  def generate_listings
+    50.times do |i|
+      listing = Listing.create!(
+        title: Faker::Name.title,
+        description: Faker::Lorem.paragraph(3),
+        pay_rate: random_pay_rate,
+        employment_type: random_employment_type,
+        business_id: rand(50),
+        closing_date: random_closing_date
+      )
+      puts "Listing #{i}: #{listing.title} - #{listing.closing_date}"
+    end
+  end
+
+  def random_pay_rate
+    random_rate = ["hr", "yr"]
+    if random_rate.sample == "yr"
+      (30000..450000).to_a.sample.to_s + "yr"
+    else
+      (9..45).to_a.sample.to_s + "hr"
+    end
+  end
+
+  def random_employment_type
+    ["full-time", "part-time"].sample
+  end
+
+  def random_closing_date
+    Time.now + (25000..1000000).to_a.sample
+  end
+
+
+
 
 end
 
