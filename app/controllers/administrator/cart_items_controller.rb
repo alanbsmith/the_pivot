a68e450c84bd18d73eba1c9baa9a804808da571a
Alan Smith <a.bax.smith@gmail.com>
2014-09-17 15:44:47 -0600

    remove admin controllers

--- app/controllers/administrator/cart_items_controller.rb
+++
@@ -1,8 +0,0 @@
-class Administrator::CartItemsController < AdminsController
-  def destroy
-    @order = Order.find(params[:id])
-    @cart_item = CartItem.find(params[:id])
-    @cart_item.destroy
-    redirect_to administrator_orders_path
-  end
-end
