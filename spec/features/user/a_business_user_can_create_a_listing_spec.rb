1209d1cebe492a28737a0665eb3fa25233767561
Horacio Chavez <horacio.hch@gmail.com>
2014-09-20 01:17:42 -0600

    Added test to validates that a user can only edit its own listings

--- spec/features/user/a_business_user_can_create_a_listing_spec.rb
+++ spec/features/user/a_business_user_can_create_a_listing_spec.rb
@@ -2,48 +2,48 @@ require 'rails_helper'
 require 'capybara/rails'
 require 'capybara/rspec'
 
-user = User.new
-  user.first_name            = "Jon"
-  user.last_name             = "Doe"
-  user.email                 = "jd@example.com"
-  user.password              = "password"
-  user.password_confirmation = "password"
-  user.save
 
 describe 'user', type: :feature do
 
-  before do
+  it 'I can create a new listing' do
+    page.visit new_listing_path
+    page.fill_in      "Title",       with: "barista"
+    page.fill_in      "Description", with: "Grinding dem beans"
+    page.fill_in      "Pay rate",    with: "8.00/hr"
+    page.choose       "part-time"
+    page.click_button "Submit"
+    save_and_open_page
+    expect(page).to have_content("barista")
+  end
+
+  xit "A user can edit its own listings" do
+    user_create
+    user_signin
+    page.visit new_listing_path
+    page.fill_in "Title", with: "Barista"
+    page.fill_in "Description", with: "Grinding dem beans"
+    page.fill_in "Pay rate", with: "8.00/hr"
+    page.choose "part-time"
+    page.click_button "Submit"
+    page.visit listings_path
+    expect(page).to have_content("Job")
+  end
+
+  def user_create
+    user = User.new
+    user.first_name            = ""
+    user.last_name             = "Doe"
+    user.email                 = "jd@example.com"
+    user.password              = "password"
+    user.password_confirmation = "password"
+    user.save
+  end
+
+  def user_signin
     page.visit '/signin'
     page.fill_in "session_email", with: "jd@example.com"
     page.fill_in "session_password", with: "password"
     page.click_link "Sign In"
-  end
-
-  it 'I can create a new listing' do
-    # as a business after I login
-    # I can click on a create listing button
-    page.visit new_listing_path
-    # I fill in the appropriate information
-    page.fill_in "Title", with: "Barista"
-    page.fill_in "Description", with: "Grinding dem beans"
-    page.fill_in "Pay rate", with: "8.00/hr"
-    page.choose "part-time"
-    # I click the submit button
-    # When i got to my listing page I expect to see that job listing
-    page.click_button "Submit"
-    page.visit listings_path
-    expect(page).to have_content("Barista")
-  end
-
-  it "A user can edit its own listings" do
-    page.visit new_listing_path
-    page.fill_in "Title", with: "Barista"
-    page.fill_in "Description", with: "Grinding dem beans"
-    page.fill_in "Pay rate", with: "8.00/hr"
-    page.choose "part-time"
-    page.click_button "Submit"
-    page.visit listings_path
-    expect(page).to have_content("Barista")
   end
 
 end
