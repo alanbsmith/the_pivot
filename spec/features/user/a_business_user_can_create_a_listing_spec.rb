9d1432772120cfa976f5fa8207f2bdf0578817ca
Horacio Chavez <horacio.hch@gmail.com>
2014-09-20 10:49:12 -0600

    Added test 'A user can create a new listing'. All tests are passing

--- spec/features/user/a_business_user_can_create_a_listing_spec.rb
+++ spec/features/user/a_business_user_can_create_a_listing_spec.rb
@@ -6,44 +6,38 @@ require 'capybara/rspec'
 describe 'user', type: :feature do
 
   it 'I can create a new listing' do
-    page.visit new_listing_path
+    user_register_a
+    add_listing
+    expect(page).to have_content("barista")
+  end
+
+  it "A user can edit its own listings" do
+    user_register_a
+    add_listing
+    expect(page).to have_content("barista")
+  end
+
+  def user_register_a
+    page.visit root_path
+    page.click_link "Register Now"
+    page.fill_in "user_company_name", with: "Apple Incorporated"
+    page.fill_in "user_first_name", with: "Steve"
+    page.fill_in "user_last_name", with: "Jobs"
+    page.fill_in "user_email", with: "steve@example.com"
+    page.fill_in "user_password", with: "password"
+    page.fill_in "user_password_confirmation", with: "password"
+    page.click_button "register_now_btn"
+  end
+
+  def add_listing
+    page.click_link "add-job-listing"
     page.fill_in      "Title",       with: "barista"
     page.fill_in      "Description", with: "Grinding dem beans"
     page.fill_in      "Pay rate",    with: "8.00/hr"
     page.choose       "part-time"
     page.click_button "Submit"
-    save_and_open_page
-    expect(page).to have_content("barista")
   end
 
-  xit "A user can edit its own listings" do
-    user_create
-    user_signin
-    page.visit new_listing_path
-    page.fill_in "Title", with: "Barista"
-    page.fill_in "Description", with: "Grinding dem beans"
-    page.fill_in "Pay rate", with: "8.00/hr"
-    page.choose "part-time"
-    page.click_button "Submit"
-    page.visit listings_path
-    expect(page).to have_content("Job")
-  end
 
-  def user_create
-    user = User.new
-    user.first_name            = ""
-    user.last_name             = "Doe"
-    user.email                 = "jd@example.com"
-    user.password              = "password"
-    user.password_confirmation = "password"
-    user.save
-  end
-
-  def user_signin
-    page.visit '/signin'
-    page.fill_in "session_email", with: "jd@example.com"
-    page.fill_in "session_password", with: "password"
-    page.click_link "Sign In"
-  end
 
 end
