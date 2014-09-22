62036ad7139041a6dc5d3170048da87820e47122
Horacio Chavez <horacio.hch@gmail.com>
2014-09-21 21:58:15 -0600

    Added link functionality to Home

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   match "/home",    to: "static_pages#home",    via: "get"
   match "/about",   to: "static_pages#about",   via: "get"
   match "/contact", to: "static_pages#contact", via: "get"
+  match "/terms-of-use", to: "static_pages#terms-of-use", via: "get"
 
   match "/register", to: "users#new",          via: "get"
   match "/signin",   to: "sessions#new",       via: "get"
